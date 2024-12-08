c674c660d4de1240ac0b528c8de416c2f7ae5709
JRosSx91 <hazee.1491@gmail.com>
2024-12-08 15:29:17 +0100

    creating main.rs initial functions

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -1,21 +1,20 @@
-[bits 16]                ; Usar modo real de 16 bits
-[org 0x7C00]             ; Dirección en memoria donde se carga el bootloader
+[bits 16]              
+[org 0x7C00]            
 
 start:
-    mov si, msg          ; Apuntar al mensaje
+    mov si, msg          
 print:
-    lodsb                ; Cargar un byte en AL desde [SI]
-    cmp al, 0            ; Comprobar si es el final del string
-    je load_kernel       ; Saltar si es el final
-    mov ah, 0x0E         ; Función para mostrar caracteres en la pantalla
-    int 0x10             ; Interrupción de BIOS
-    jmp print            ; Repetir para el siguiente carácter
+    lodsb                
+    cmp al, 0            
+    je load_kernel       
+    mov ah, 0x0E         
+    int 0x10             
+    jmp print            
 
 load_kernel:
-    ; Aquí se cargaría el kernel desde un sector del disco a la memoria (se simplifica en esta etapa)
-    jmp $                ; Bucle infinito (placeholder)
+    jmp $                
 
-msg db 'Hello, World!', 0 ; Mensaje a mostrar
+msg db 'Hello, World!', 0 
 
-times 510-($-$$) db 0     ; Rellenar hasta los 510 bytes
-dw 0xAA55                 ; Firma del bootloader
+times 510-($-$$) db 0     
+dw 0xAA55                 
